b5735beea38cbb4bdbdd12c1a3135fc6a63a5562
yanzhang1125 <31359006+yanzhang1125@users.noreply.github.com>
2017-11-01 21:14:38 -0700

    Final

--- week11.R
+++ week11.R
@@ -54,8 +54,6 @@ occurance[1]
 occurance[[1]]
 occurance[[1]][1]
 
-
-
 ###5
 
 str_sub(string = "testing", start = 1, end = 3)
@@ -119,14 +117,83 @@ str_locate(emails[1],
 ###8
 
 #header
-header1 = str_sub(emails[1],end = 842)
+metadata1 = str_sub(emails[1],end = 842)
 #up to you to decide the end, to put the lines in the body or header
 
 #body
 body1 = str_sub(emails[1],843)
 
+cat(metadata1)
+cat(body1)
+
+
+###9
+
+str_split(string = metadata1, pattern = "\n")
+#list
+
+###10
+
+breaks = str_locate(emails,pattern = "\n\n") #gives you the dataframe
+#the ones with NA we need to do it mannuly
+
+metadata = str_sub(emails,end =breaks[,1])
+body = str_sub(emails, start = breaks[,2])
+
+#before commit, make sure saving
+
+###second page
+###1
+
+fruit = c("apple", "banana", "pear", "pinapple")
+
+
+###2
+str_detect(fruit, "a")
+str_detect(fruit, "^a") #first letter
+str_detect(fruit, "a$") # ends with this pattern
+str_detect(fruit, "[aeiou]")  #[] is or
+#in regular expression, every one is "or" relation
+str_detect(fruit, "[a-d]") #a to d
+
+###3
+#detect a string that start with a and ends with "e"
+str_detect(fruit,"^a[a-z]z$")
+#doesnt work, looking for three characters
+
+str_detect(fruit,"^a[a-z]*e$")
+
+#? none or one, eith 2 or 3 characters
+
+###4
+
+phone = "213 740 4826"
+
+parser = "[0-9]{3} [0-9]{3} [0-9]{4}"
+
+str_detect(phone, parser)
+
+phone2 = c("213 740 4826", "213-740-4826","213,740,4826", "(213) 705-4826", "(213) 7054826")
+
+parser = "[(]?[0-9]{3}[)]?[ ,-][0-9]{3}[ ,-][0-9]{4}"
+parser = "[(]?[0-9]{3}[)]?[ ,-][0-9]{3}[ ,-][0-9]{4}\\b" #how it should end with
+str_detect(phone2, parser)
+
+#( should be in the []
+
+###5
+cat(body[10])
+cat(body[18])
+
+str_extract(string = body, pattern = parser)
 
 
 
 
+####6
+
+zip = c("90028", "90028-0809")
+paser2 = "[0-9]{5}[-]?([0-9]{4})?"             
+#group with()
+str_detect(zip, paser2)
 
